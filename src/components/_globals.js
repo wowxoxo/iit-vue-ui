2c63518a88503b3e361ea6ec8ebe63bb5f273f53
Tsivinsky.Petr <creatsiv@gmail.com>
2019-01-17 12:20:56 +0300

    Changes for v0.0.13

--- src/components/_globals.js
+++ src/components/_globals.js
@@ -1,20 +1,29 @@
 import Vue from "vue";
-import upperFirst from "lodash/upperFirst";
-import camelCase from "lodash/camelCase";
+// import upperFirst from "lodash/upperFirst";
+// import camelCase from "lodash/camelCase";
 import eventBus from "@/helpers/eventBus";
+import IitCheckbox from "./IitCheckbox.vue";
 
-const requireComponent = require.context(".", false, /Iit[\w-]+.vue$/);
+// const requireComponent = require.context(".", false, /Iit[\w-]+.vue$/);
 
-requireComponent.keys().forEach(filename => {
-  // Get Component Config
-  const componentConfig = requireComponent(filename);
+// requireComponent.keys().forEach(filename => {
+//   // Get Component Config
+//   const componentConfig = requireComponent(filename);
 
-  // Get PascalCase name of component
-  const componentName = upperFirst(
-    camelCase(filename.replace(/^\.\//, "").replace(/\.\w+$/, ""))
-  );
+//   // Get PascalCase name of component
+//   const componentName = upperFirst(
+//     camelCase(filename.replace(/^\.\//, "").replace(/\.\w+$/, ""))
+//   );
 
-  Vue.component(componentName, componentConfig.default || componentConfig);
+//   Vue.component(componentName, componentConfig.default || componentConfig);
+// });
+
+const Components = {
+  IitCheckbox
+};
+
+Object.keys(Components).forEach(name => {
+  Vue.component(name, Components[name]);
 });
 
 export default eventBus;
